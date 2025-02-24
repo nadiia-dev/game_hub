b7ced0f0115146c898b10e6b7d7b7ecff1be5655
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-02-24 16:47:14 +0200

    added game details, trailer and screenshots

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "@/components/ui/color-mode";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <HStack justifyContent={"space-between"} paddingX="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/game_hub/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeButton />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,14 +4,19 @@ import { BsSearch } from "react-icons/bs";
 import { useRef } from "react";
 import { useColorModeValue } from "./ui/color-mode";
 import useGameQueryStore from "@/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (ref.current) setSearchText(ref.current.value);
+    if (ref.current) {
+      setSearchText(ref.current.value);
+      navigate("/game_hub/");
+    }
   };
 
   const bg = useColorModeValue("gray.100", "gray.700");
